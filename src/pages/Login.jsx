f2ef93ee4515d36974f0b50ad6002578a97df280
handongIMac <handong@handongIMacs-iMac.local>
2023-11-29 20:28:01 +0800

    fix no use

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,9 @@
 import request from '../util/request'
 function Login() {
     const login = () => {
-        let payload = {
-            email: 'admin',
-            password: '123456'
-        }
         request.get('User').then((res) => {
 
         })
-
     }
     return (
         <>Login Page
